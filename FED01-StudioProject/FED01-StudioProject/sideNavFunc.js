dda458a077270d72239ecab87d6b8450ab85c3a6
Taylor Griffin <titleist315@gmail.com>
2021-04-01 08:33:46 -0500

    completed side nav

---
+++ FED01-StudioProject/FED01-StudioProject/sideNavFunc.js
@@ -0,0 +1,11 @@
+﻿
+
+function openSideNav() {
+    document.getElementById('class-btn').style.display = 'none';
+    document.getElementById('side-nav').style.width = '200px';
+}
+
+function closeSideNav() {
+    document.getElementById('class-btn').style.display = 'block';
+    document.getElementById('side-nav').style.width = '0px';
+}
