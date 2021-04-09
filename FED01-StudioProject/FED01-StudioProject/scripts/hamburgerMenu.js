c9cbe687871e2e81dbfe79d67fa7d3212eac39e8
Taylor Griffin <titleist315@gmail.com>
2021-04-09 15:10:14 -0500

    finished the home page

---
+++ FED01-StudioProject/FED01-StudioProject/scripts/hamburgerMenu.js
@@ -0,0 +1,14 @@
+﻿
+
+function toggleNav() {
+    let navList = document.getElementById('nav-list');
+    navList.classList.toggle('expanded');
+
+    let navBtn = document.getElementById('nav-btn');
+    if (navList.classList.contains('expanded')) {
+        navBtn.innerHTML = '&times';
+    }
+    else {
+        navBtn.innerHTML = '&#9776';
+    }
+}
